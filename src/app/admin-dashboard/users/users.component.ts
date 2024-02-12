2d3be359ef13d1b152d22ec649da5b354308a40c
RamandeepSidhu <117151940+RamandeepSidhu@users.noreply.github.com>
2024-02-12 15:36:43 +0530

    edit table

--- src/app/admin-dashboard/users/users.component.ts
+++ src/app/admin-dashboard/users/users.component.ts
@@ -13,14 +13,14 @@ import { UserService } from 'src/app/core/services/users.service';
 export class UsersComponent  implements OnInit {
   public users: any = [];
   public isLoading = false;
-  private timeoutId: any;
   public usersData: any;
   public registrationForm!: FormGroup;
   public submitted = false;
+  public updateSubmitted = false;
   public isNewPassword = false;
-  selectedRole:any = RoleEnum.CLIENT; 
   isEditing: boolean = false;
   isEdit: boolean=false;
+  isAddingNewRecord: boolean = false;
   constructor(
     private userServices: UserService,
     private toaster: ToastrService,
@@ -29,27 +29,17 @@ export class UsersComponent  implements OnInit {
   ) { }
   ngOnInit(): void {
     this.getUsers()
+    this.initForm();
+  }
+  initForm(): void {
     this.registrationForm = this.formBuilder.group({
       name: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
-      password: [''],
       phone:[''],
       linkedin: [''],
-      role: [RoleEnum.CLIENT],
       country: ['India'],
     });
-    this.registrationForm.get('role')?.valueChanges.subscribe((selectedRole: Number) => {
-      this.selectedRole = selectedRole;
-      if (selectedRole === RoleEnum.ADMIN) {
-        this.registrationForm.get('password')?.setValidators([Validators.required]);
-      } else {
-        this.registrationForm.get('password')?.clearValidators();
-        this.registrationForm.get('password')?.patchValue('');
-      }
-      this.registrationForm.get('password')?.updateValueAndValidity();
-    });
   }
-
   getUsers(params?:any) {
     this.isLoading = true;
     try {
@@ -71,61 +61,51 @@ export class UsersComponent  implements OnInit {
       this.isLoading = false;
     }
   }
-  selectRole(event:any){
-    const { value } = event.target;
-    this.getUsers(value?{ role:value }:'');
-  }
 
-  onSearchChange(event:any){
-    const { value } = event.target;
-    if (this.timeoutId) {
-      clearTimeout(this.timeoutId);
-    }
-    this.timeoutId = setTimeout(() => {
-      this.getUsers(value?{ name:value }:'');
-    }, 500);
-  }
 
-  calculateWidth(content: string): string {
-    const minWidth = '50px';
-    const textLength = content ? content.length : 0;
-    const width = Math.max(textLength * 10, parseInt(minWidth, 10));
-    return width + 'px';
-  }
-
-  userEdit(user:any,index:number){
-    this.users[index].isDisabled = false;
-    this.cancelEdit();
-    this.users.forEach((element: any) => {
-     this.isEdit = false;
+  userEdit(user:any, index:number){
+    user.isEdit = true; 
+    user.isDisabled = false;
+    this.users.forEach((element: any, i: number) => {
+        if (i !== index) {
+            element.isDisabled = true; 
+        }
     });
+    this.isEdit = true;
+}
+
+updateUser(user: any, index: number) {
+  if (!this.registrationForm.valid) {
+    this.users[index].isDisabled = false;
     user.isEdit = true;
+    this.updateSubmitted = true;
+    this.isEdit = true;
+    return;
   }
-
-  updateUser(user:any,index:number){
-    this.users[index].isDisabled = true;
-  }
-
-  deleteUser(user:any,index:number){
-    this.isLoading = true;
-    this.userServices.removeUser(user._id).subscribe(
+  this.users[index].isDisabled = true;
+    this.registrationForm.patchValue({
+    name: user.name,
+    email: user.email,
+    phone: user.phone,
+    linkedin: user.linkedin,
+    country: user.country,
+  });
+  const payload = { ...this.registrationForm.value};
+    delete payload.password;
+    this.userServices.userUpdate(user._id, payload).subscribe(
       (response: any) => {
         if (response.status === true) {
+          user.isEdit = false;
+          this.isEdit = false;
+          this.updateSubmitted = false;
           this.toaster.success(response.message, 'Success');
-          this.users.splice(index,1);
-          this.isLoading = false;
-        }
-        else{
+        } else {
           this.toaster.error(response.message, 'Error');
         }
-      },
+        this.isLoading = false;
+      }
     );
-  }
-
-  logout() {
-    localStorage.clear();
-    this.route.navigate(['/login']);
-  }
+}
 
   submitRegistration() {
     this.isLoading = true;
@@ -150,6 +130,8 @@ export class UsersComponent  implements OnInit {
             this.registrationForm.reset();
             this.registrationForm.get('role')?.patchValue(RoleEnum.CLIENT);
             this.registrationForm.get('country')?.patchValue('India');
+            this.isAddingNewRecord = false;
+            this.isEdit = false;
           }
           else{
             this.toaster.error(response.message, 'Error');
@@ -158,31 +140,32 @@ export class UsersComponent  implements OnInit {
       );
   }
 
-
-
+  deleteUser(user:any,index:number){
+    this.isLoading = true;
+    this.userServices.removeUser(user._id).subscribe(
+      (response: any) => {
+        if (response.status === true) {
+          this.toaster.success(response.message, 'Success');
+          this.users.splice(index,1);
+          this.isLoading = false;
+          this.isEdit = false;
+        }
+        else{
+          this.toaster.error(response.message, 'Error');
+        }
+      },
+    );
+  }
 
   addNewRecord(): void {
-    const newRecord = {
-      name: '',
-      email: '',
-      phone: '',
-      linkedin: '',
-      country: '',
-      platform:'',
-      lead_score:'',
-      conversion:'',
-      isEdit: true,
-    };
-
-    this.users.unshift(newRecord);
+    if (this.isEdit) {
+      this.isEdit = false;
+      this.users.forEach((element: any) => {
+        element.isEdit = false;
+      });
+    }
+    this.isAddingNewRecord = true;
+    this.initForm();
   }
-
-  cancelEdit(): void {
-    this.users.forEach((element: any) => {
-      element.isEdit = false;
-    });
-
-    this.isEditing = false;
-  }
-
+  
 }
